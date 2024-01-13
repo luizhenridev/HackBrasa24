2dd03a1537d4d928fdcf9fee56f3fa01d2e31499
Luiz Cavalcante <luiz.cavalcante@ifood.com.br>
2024-01-12 23:17:04 -0300

    Migrations - created_at

--- migrations/create_user_table.sql
+++ migrations/create_user_table.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS "user" (
     user_id UUID NOT NULL,
     name TEXT,
     cellphone_number TEXT,
+    created_at DATE,
     CONSTRAINT user_id_pkey PRIMARY KEY (user_id)
 );
