d602a6d669685ce80d0209ff0f82ac3ce41bd1b0
Luiz Cavalcante <luiz.cavalcante@ifood.com.br>
2024-02-11 01:01:17 -0300

    New Migrations

--- migrations/create_spending_table.sql
+++ migrations/create_spending_table.sql
@@ -1,9 +1,7 @@
-CREATE TABLE IF NOT EXISTS "gasto"(
+CREATE TABLE IF NOT EXISTS "spending"(
     spend_id UUID NOT NULL PRIMARY KEY,
+    user_id INT not null,
     description_spend VARCHAR,
-    value_spend FLOAT
-    CONSTRAINT fk_user
-        FOREIGN KEY(user_id)
-        REFERENCES "user"(user_id)
-        ON DELETE CASCADE
-)
+    value_spend FLOAT,
+    CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES "user"(user_id) ON DELETE CASCADE
+);
